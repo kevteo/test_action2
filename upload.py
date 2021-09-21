7bc7d79399891a0b13fd547cf6d955032122f9df
kevteo <31839547+kevteo@users.noreply.github.com>
2021-09-21 12:12:49 +0800

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,1 +1,12 @@
+import os
 
+username = 'ai-sdk'
+os.system("git config --global user.name " + str(username))
+os.system("git config --global user.email " + str(username) + '@users.noreply.github.com')
+print('Status:')
+print(os.system('git status'))
+os.system('git add -A')
+print('Status: 2')
+print(os.system('git status'))
+os.system('git commit -m Produce Merge File')
+os.system('git push origin main')
