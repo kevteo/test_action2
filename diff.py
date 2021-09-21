aa8767e4d5f9dbb2f06788af329d6d140ace45a0
kevteo <31839547+kevteo@users.noreply.github.com>
2021-09-21 16:50:38 +0800

    Update diff.py

--- diff.py
+++ diff.py
@@ -44,14 +44,14 @@ def main():
         json.dump(base, f, ensure_ascii=False, indent=4)
     
     # Push to Git
-#     username = 'ai-sdk'
-#     os.system("git config --global user.name " + str(username))
-#     os.system("git config --global user.email " + str(username) + '@users.noreply.github.com')
-#     print('Status:')
-#     print(os.system('git status'))
-#     os.system('git add -A')
-#     os.system('git commit -m Produce Merge File')
-#     os.system('git push origin main')
+    username = 'ai-sdk'
+    os.system("git config --global user.name " + str(username))
+    os.system("git config --global user.email " + str(username) + '@users.noreply.github.com')
+    print('Status:')
+    print(os.system('git status'))
+    os.system('git add -A')
+    os.system('git commit -m Produce Merge File')
+    os.system('git push origin main')
 
 
 
