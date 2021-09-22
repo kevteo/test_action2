909b2a3e7f2a6bedd2f57cc4ecafdbef20031952
kevteo <31839547+kevteo@users.noreply.github.com>
2021-09-23 02:27:53 +0800

    Update diff.py

--- diff.py
+++ diff.py
@@ -1,6 +1,6 @@
 import os
 import json
-# from apps.util import json_merge, generate_number_changes
+# from apps.util import json_merge
 from flatten_json import flatten
 from jsonmerge import Merger
 from jsondiff import diff
@@ -11,19 +11,6 @@ def json_merge(base, new, merge_strategy):
     merger = Merger(schema)
     base = merger.merge(base, new)
     return base
-
-def generate_number_changes(difference_history):
-    num_changes = {}
-    for difference in difference_history:
-        if type(difference) is list:
-            continue
-        
-        for key in difference.keys():
-            if key in num_changes:
-                num_changes[key] +=1 
-            else:
-                num_changes[key] = 1
-    return num_changes
 
 def main():
     # Read Inputs
@@ -45,20 +32,17 @@ def main():
     base = json_merge(base, json1, merge_strategy)
     base = json_merge(base, json2, merge_strategy)
     difference = diff(json1, json2, syntax='symmetric', marshal=True)
-    num_changes = generate_number_changes(difference)
     
     # Write Files
     with open('merged_json.json', 'w', encoding='utf-8') as f:
         json.dump(base, f, ensure_ascii=False, indent=4)
     with open('difference.json', 'w', encoding='utf-8') as f:
         json.dump(difference, f, ensure_ascii=False, indent=4)
-    with open('num_changes.json', 'w', encoding='utf-8') as f:
-        json.dump(num_changes, f, ensure_ascii=False, indent=4)
     
     # Push to Git
     repo = Repo('.')
     repo.git.reset()
-    repo.index.add(['merged_json.json', 'difference.json', 'num_changes.json'])
+    repo.index.add(['merged_json.json', 'difference.json'])
     repo.index.commit('Upload Merged Json')
     origin = repo.remote('origin')
     origin.push()
